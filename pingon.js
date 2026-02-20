a54a4f4cca2cfe09c297912c95ab7b0c8fe01817
Gustavo <gustavo.ferraz.coelho@escola.pr.gov.br>
2026-02-20 07:46:58 -0300

    update

---
+++ pingon.js
@@ -0,0 +1,19 @@
+let raio = 7.5;
+let xbolinha = 100;
+let ybolinha = 200;
+let diametro = raio * 2
+let velocidadexbolinha = 6;
+let velocidadeybolinha = 6;
+let xraquete = 5;
+let yraquete = 150;
+let xraqueteoponente = 585;
+let yraqueteoponente = 150;
+let meuspontos = 0;
+let pontosoponente = 0;
+
+function setup(){
+  createCanvas(600,400)
+}
+function draw(){
+  
+}
