ad3d525f29fcf8ca790df7388dc262e34e0457f6
Gustavo <gustavo.ferraz.coelho@escola.pr.gov.br>
2026-02-27 07:55:55 -0300

    update

--- pingon.js
+++ pingon.js
@@ -10,12 +10,29 @@ let xraqueteoponente = 585;
 let yraqueteoponente = 150;
 let meuspontos = 0;
 let pontosoponente = 0;
+let dificuldade = "normal"
+let trilha,ponto,raquetada;
+function preload(){
+  trilha = loadSound("trilha.mp3")
+  ponto = loadSound("ponto.mp3")
+  raquetada = loadSound("raquetada.mp3")
+}
 
 function setup(){
   createCanvas(600,400)
+  trilha.loop()
 }
 function draw(){
-  
+  background(0)
+  movimentobolinha();
+  raquetes(xraquete,yraquete)
+  raquetes(xraqueteoponete,yraqueteoponente)
+  npc(dificuldade)
+  if(keyIsDown(UP_ARROW)){
+    yraquete -= 10}
+  if(xbolinha -raio < xraquete + 10 & ybolinha -raio < yraquete + 90 & ybolinha +raio > yraquete ){
+    velocidadexbolinha *= -1;
+    raquetada.play()}
 }
 
 function movimentoBolinha(){
